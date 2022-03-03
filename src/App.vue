c166582a4a8618519eed77dbadb154ae66e38eb3
Russel <blokhin.rus@gmail.com>
2022-03-03 16:28:33 +0300

    add search

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,39 @@
 export default {
   data() {
     return {
-      message: "Hello World!",
+      q: "",
+      data: null,
+      users: [],
     };
   },
+  methods: {
+    async fetchData() {
+      this.data = null;
+      const res = await fetch(
+        `https://api.github.com/search/users?q=${this.q}+in:login`
+      );
+      this.data = await res.json();
+      console.log(this.data.items);
+    },
+  },
+
+  // mounted() {
+  //   this.fetchData();
+  // },
+  // watch: {
+  //   todoId() {
+  //     this.fetchData();
+  //   },
+  // },
 };
 </script>
+
 <template>
-  <h1>{{ message }}</h1>
+  <h2>Enter login:</h2>
+  <input v-model="q" @keyup.enter="fetchData" />
+  <button @click="fetchData">Search</button>
+  <p v-if="!data">No result</p>
+  <ul v-else>
+    <li v-for="user in data.items" :key="user.id">{{ user.login }}</li>
+  </ul>
 </template>
